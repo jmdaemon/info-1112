f4dd988f25dcb034cbf6105f649188c7ae89d9c0
Joseph Diza <josephm.diza@gmail.com>
2023-05-11 10:24:30 -0700

    Complete first program in course

---
+++ ch01/p01.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::cout << "*************************************************" << std::endl;
+  std::cout << "*                   Welcome to C++              *" << std::endl;
+  std::cout << "*************************************************" << std::endl;
+  return 0;
+}
