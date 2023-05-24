510c1d0ab867b406e3a0b9bf24bb2b09ce487814
Joseph Diza <josephm.diza@gmail.com>
2023-05-23 21:52:42 -0700

    Complete question 7

---
+++ week02/q7.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include <cmath>
+#include <tuple>
+
+auto get_user_input() {
+  int time;
+  std::cout << "Enter the elapsed time in whole seconds: ";
+  std::cin >> time;
+  return time;
+}
+
+auto seconds_to_24hr(int seconds) {
+  double remainder = seconds / 3600.0;
+  int hh = floor(remainder);
+
+  remainder = ((remainder - hh) * 3600) / 60.0;
+  int mm = floor(remainder);
+
+  remainder = ((remainder - mm) * 3600) / 60.0;
+  int ss = round(remainder);
+
+  //int mm = (int) ((seconds - (hh * 3600)) / 60);
+  //int ss = (int) (seconds / 60);
+
+  //int mm = (int) ((seconds - (hh * 3600) % 60));
+  //int ss = (int) (seconds / 60);
+
+  //int mm = (int) ((seconds - (hh * 3600)) / 60);
+  //int ss = (int) (seconds / 60);
+
+
+
+  return std::make_tuple(hh, mm, ss);
+}
+
+void show_elapsed_time(int seconds) {
+  auto [hh,mm,ss] = seconds_to_24hr(seconds);
+  const char* display_format = "%d:%d:%d\n";
+  printf(display_format, hh, mm, ss);
+}
+
+int main() {
+  show_elapsed_time(get_user_input());
+}
