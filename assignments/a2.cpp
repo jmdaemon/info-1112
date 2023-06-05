be0e9e34a5278096838119ded10f7457770a159f
Joseph Diza <josephm.diza@gmail.com>
2023-06-04 18:58:39 -0700

    Prune dead code

--- assignments/a2.cpp
+++ assignments/a2.cpp
@@ -41,34 +41,16 @@ double mi_to_km(double miles) {
 
 double to_km(char units, double distance) {
   return (units == 'M') ? mi_to_km(distance) : distance;
-  //double km = (units == 'M') ? distance : mi_to_km(distance);
-  //return km;
-
-  //return units == 'M' ? mi_to_km(distance) : distance;
-  //return ((toupper(units) == 'M') ? mi_to_km(distance) : distance);
-  //switch(toupper(units)) {
-    //case 'M': distance = mi_to_km(distance); break;
-  //}
 }
 
-//int calc_time(double distance, double speed) {
 double calc_time(double distance, double speed) {
   return distance / speed;
-  //return distance * speed;
 }
 
 // Show the trip and transportation information
-//void show_trip(double km, double speed, int eta) {
 void show_trip(double km, double speed, double eta) {
-  // Set decimal number formatting
-  //std::cout << std::fixed << std::showpoint << std::setprecision(3);
-  //std::cout << "Your final destination is " << ":" << km << " kilometers away.\n";
-  //std::cout << "Your estimated time of arrival for your trip is " << ":" << time << " minutes.\n";
-  //std::cout << "For your vehicle traveling " << ":"  << speed << " kilometers/minute.\n";
-
   puts("\nTrip Stats:");
   printf("\tYour final destination is %.3f km away.\n", km);
-  //printf("\tYour estimated time of arrival is %d minutes.\n", eta);
   printf("\tYour estimated time of arrival is %.f minutes.\n", eta);
   printf("\tYour vehicle is traveling at %.3f km/min.\n", speed);
 }
@@ -76,11 +58,8 @@ void show_trip(double km, double speed, double eta) {
 int main() {
   auto [units, distance, speed] = get_user_input();
   
-
   // Convert distance to kilometers if miles are given
   double km = to_km(units, distance);
-
-  std::cout << units << distance << speed;
 
   // Calculate the estimated time of arrival for the trip
   double eta = calc_time(km, speed);
