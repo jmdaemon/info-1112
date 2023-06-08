8cdde352df0d3b81b94e185f80c796335f84bdee
Joseph Diza <josephm.diza@gmail.com>
2023-06-08 12:29:16 -0700

    Test invalid cases too

--- labs/l2/test.sh
+++ labs/l2/test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+AMOUNT=1000
+
+# Test correct cases
 for i in $(seq 0 10); do
-    echo "1000 $i" | ./bin/l2
+    echo "$AMOUNT $i" | ./bin/l2
 done
+
+# Test invalid cases
+echo "$AMOUNT -1" | ./bin/l2
+echo "$AMOUNT 11" | ./bin/l2
