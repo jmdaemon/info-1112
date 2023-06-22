52a96905be202e8f07c3cbaca73a5680f21ec0d6
Joseph Diza <josephm.diza@gmail.com>
2023-06-22 11:31:15 -0700

    Build with switch statement for lab

--- week05/q1.cpp
+++ week05/q1.cpp
@@ -2,25 +2,17 @@
 #include <iomanip>
 #include <cstdio>
 
-// Determine if a value is in a specified range (inclusive)
-bool in_range_i(int min, int max, int val) {
-  return (val >= min && val <= max);
-}
-
 void populate_employee_ages(int employee_ages[4]) {
   int age = 0;
   while (age >= 0) {
     printf("Enter an employee age. (Note: To stop reading data, enter a negative integer): ");
     std::cin >> age;
 
-    if (in_range_i(0, 30, age)) {
-      employee_ages[0]++;
-    } else if (in_range_i(30, 40, age)) {
-      employee_ages[1]++;
-    } else if (in_range_i(41, 50, age)) {
-      employee_ages[2]++;
-    } else if (age > 50) {
-      employee_ages[3]++;
+    switch(age / 10) {
+      case(1): case(2): employee_ages[0]++; break;
+      case(3): employee_ages[1]++; break;
+      case(4): employee_ages[2]++; break;
+      case(5): employee_ages[3]++; break;
     }
   }
 }
@@ -30,10 +22,10 @@ void show_employee_ages(int employee_ages[4]) {
   auto print_col = [](int spaces, auto text) {
     std::cout << std::left << std::setw(spaces) << text;
   };
-  auto print_gapsize_8 = [&](auto text) { print_col(8, text); };
+  auto print_gapsize_10 = [&](auto text) { print_col(10, text); };
   auto print_gapsize_4 = [&](auto text) { print_col(4, text); };
   auto print_row = [&](auto col1, auto col2) {
-    print_gapsize_8(col1); print_gapsize_4(col2); puts("");
+    print_gapsize_10(col1); print_gapsize_4(col2); puts("");
   };
   
   std::cout << "Number of Employees in each Age Group" << std::endl;
