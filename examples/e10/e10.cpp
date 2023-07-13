308a347776d3513de5862a81c1784cda7f8807c1
Joseph Diza <josephm.diza@gmail.com>
2023-07-13 12:07:12 -0700

    Prune dead code

--- examples/e10/e10.cpp
+++ examples/e10/e10.cpp
@@ -5,27 +5,14 @@ void set_random_seed() {
   srand(time(0));
 }
 
-//int gen_random_number(const NatNumber min, const NatNumber max) {
-  //return rand() % (max - min + 1) + min;
-//}
-
 // Generate a random double between 0.0 and 1.0
-double gen_random_distribution(double min, double max) {
-  double distribution = ((double) rand() / (double) RAND_MAX);
-  return distribution * max;
+double gen_random_distribution() {
+  return ((double) rand() / (double) RAND_MAX);
 }
 
 // Generate a random double between min and max
 double gen_random_double(double min, double max) {
-  //return (rand() / MAX) * MAX;
-  //return rand() / (max - min) + min;
-  //return ((double) rand() / (double) (max - min) + min);
-  //return ((double) rand() / (double) max);
-  
-  double distribution = ((double) rand() / (double) RAND_MAX);
-  //return (distribution / (max - min)) + min;
-  //return distribution * max;
-  //return (distribution * (max - min)) + min;
+  double distribution = gen_random_distribution();
   return distribution * max + min;
 }
 
@@ -37,5 +24,4 @@ int main() {
   const int n = 20;
   for (int i = 0; i < n; i++)
     printf("%f\n", gen_random_double(MIN, MAX));
-    //std::cout << gen_random_double(MAX, MIN) << std::endl;
 }
