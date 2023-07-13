f98de4225ac19c2c54d73b5f83191f50be800eed
Joseph Diza <josephm.diza@gmail.com>
2023-07-13 12:23:27 -0700

    Add min, avg funcctions

--- examples/e10/e10.cpp
+++ examples/e10/e10.cpp
@@ -18,6 +18,30 @@ double gen_random_double(double min, double max) {
   return distribution * max + min;
 }
 
+// 2: Comparing float values
+double dsum(double array[], int size) {
+  double sum = 0;
+  for (int i = 0; i < size; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
+
+double davg(double array[], int size) {
+  double sum = dsum(array, size);
+  return sum / (double) size;
+}
+
+// Find the minimum value in an array
+// If the array is empty, the minimum returned will be -1
+double d_min(double array[], int size) {
+  int min = -1;
+  for (int i = 0; i < size; i++)
+    if (min < array[i])
+      min = array[i];
+  return min;
+}
+
 int main() {
   const int MAX = 100;
   const int MIN = 2;
